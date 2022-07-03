13409b2b978a3e896ed9d90fcd4671c328636dd6
Sumit Gautam <sghost33@gmail.com>
2022-07-03 10:16:44 +1000

    Testing index, minting and contract deployment completed

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -1,34 +1,75 @@
-const {assert} = require('chai');
-const KryptoBird = artifacts.require('./KryptoBird.sol');
+const { assert } = require('chai')
+const KryptoBird = artifacts.require('./KryptoBird.sol')
 
 //check for chai
-require('chai').use(require('chai-as-promised')).should();
-
+require('chai').use(require('chai-as-promised')).should()
 
 // eslint-disable-next-line no-undef
 contract('KryptoBird', (accounts) => {
-  let contract;
+  let contract
+
+  beforeEach(async () => {
+    contract = await KryptoBird.deployed()
+  })
 
   // testing container - describe
   describe('deployment', async () => {
     // test samples with writing it
-    it('deploys successfully', async () => { 
-      contract = await KryptoBird.deployed();
-      const address = contract.address;
-      assert.notEqual(address, '');
-      assert.notEqual(address, null);
-      assert.notEqual(address, undefined);
-      assert.notEqual(address, 0x0);
+    it('deploys successfully', async () => {
+      const address = contract.address
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+      assert.notEqual(address, 0x0)
     })
 
-    it('has a name', async() => {
-      const name = await contract.name();
-      assert.equal(name, 'KryptoBird');
+    it('has a name', async () => {
+      const name = await contract.name()
+      assert.equal(name, 'KryptoBird')
     })
 
-    it('has a symbol', async() => {
-      const symbol = await contract.symbol();
-      assert.equal(symbol, 'KBIRDZ');
+    it('has a symbol', async () => {
+      const symbol = await contract.symbol()
+      assert.equal(symbol, 'KBIRDZ')
+    })
+  })
+
+  describe('minting', async () => {
+    it('creates a new token', async () => {
+      const result = await contract.mint('https...1')
+      const totalSupply = await contract.totalSupply()
+      assert.equal(totalSupply, 1)
+      const event = result.logs[0].args
+      assert.equal(
+        event._from,
+        '0x0000000000000000000000000000000000000000',
+        'from is the contract',
+      )
+      assert.equal(event._to, accounts[0], 'to is msg.sender ')
+
+      // Failure
+      await contract.mint('https...1').should.be.rejected
+    })
+  })
+
+  describe('indexing', async () => {
+    it('lists KryptoBirds', async () => {
+      // Mint three new tokens
+      await contract.mint('https...2')
+      await contract.mint('https...3')
+      await contract.mint('https...4')
+      const totalSupply = await contract.totalSupply()
+      // Loop through list and grab KBirdz from list
+      let result = []
+      let KryptoBird
+      for (let i = 1; i <= totalSupply; i++) {
+        KryptoBird = await contract.kryptoBirdz(i - 1)
+        result.push(KryptoBird)
+      }
+
+      //assert that our new array result  will equal our expected result
+      const expected = ['https...1', 'https...2', 'https...3', 'https...4']
+      assert.equal(result.join(','), expected.join(','));
     })
   })
 })
